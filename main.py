7eb378061e574c54fa7711a3c9c42daf09f84746
lfmjun <1286495117@qq.com>
2024-04-12 16:48:45 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import pyshark
+import threading
+import asyncio
+
+# 替换 'interface' 为你要抓包的网络接口，如 'eth0'、'wlan0' 等
+interface = '以太网'
+# 替换 'display_filter' 为你的显示过滤器
+display_filter = '((rtmpt) && (_ws.col.info contains "connect")) || (_ws.col.info contains "releaseStream")'
+# 替换 'tshark_path' 为你的 tshark.exe 路径
+tshark_path = "F:\\Program Files\\Wireshark\\tshark.exe"
+
+
+class PacketSniffer(threading.Thread):
+    def __init__(self, interface, display_filter):
+        super().__init__()
+        self.interface = interface
+        self.display_filter = display_filter
+
+    def run(self):
+        asyncio.set_event_loop(asyncio.new_event_loop())  # 创建并设置新的事件循环
+        capture = pyshark.LiveCapture(interface=self.interface, display_filter=self.display_filter,
+                                      tshark_path=tshark_path)
+        for packet in capture.sniff_continuously():
+            packet = str(packet)
+            server = filter_strings(packet, "rtmp://")
+            code = filter_strings(packet, "stream-")
+            if server:
+                print("服务器 ", server)
+            else:
+                print("推流码 ", code)
+
+
+def filter_strings(input_str, target_str):
+    words = input_str.split()  # 将字符串按空格分割成单词列表
+    for word in words:
+        if target_str in word:
+            return word[1:-1]  # 去除引号
+
+
+sniffer = PacketSniffer(interface, display_filter)
+sniffer.start()
+sniffer.join()
