5433fbe61105dfdff51d1f1cf119e4727d3c8b63
lfmjun <1286495117@qq.com>
2024-04-12 19:08:01 +0800

    Update config.json 可以通过json文件来修改参数

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import pyshark
 import threading
 import asyncio
+import json
 
-# 替换 'interface' 为你要抓包的网络接口，如 'eth0'、'wlan0' 等
-interface = '以太网'
-# 替换 'display_filter' 为你的显示过滤器
-display_filter = '((rtmpt) && (_ws.col.info contains "connect")) || (_ws.col.info contains "releaseStream")'
-# 替换 'tshark_path' 为你的 tshark.exe 路径
-tshark_path = "F:\\Program Files\\Wireshark\\tshark.exe"
+# 从文件中读取 JSON 数据
+with open('config.json', 'r', encoding='utf-8') as file:
+    json_data = file.read()
+
+# 解析 JSON 数据
+data = json.loads(json_data)
+
+# 获取 tshark_path、display_filter 和 interface 字段的值
+tshark_path = data["tshark_path"]
+display_filter = data["display_filter"]
+interface = data["interface"]
+
+# # 替换 'interface' 为你要抓包的网络接口，如 'eth0'、'wlan0' 等
+# interface = '以太网'
+# # 替换 'display_filter' 为你的显示过滤器
+# display_filter = '((rtmpt) && (_ws.col.info contains "connect")) || (_ws.col.info contains "releaseStream")'
+# # 替换 'tshark_path' 为你的 tshark.exe 路径
+# tshark_path = "F:\\Program Files\\Wireshark\\tshark.exe"
 
 
 class PacketSniffer(threading.Thread):
@@ -27,14 +40,14 @@ class PacketSniffer(threading.Thread):
             if server:
                 print("服务器 ", server)
             else:
-                print("推流码 ", code)
+                print("推流码 ", code[1:-1]) # 去除引号
 
 
 def filter_strings(input_str, target_str):
     words = input_str.split()  # 将字符串按空格分割成单词列表
     for word in words:
         if target_str in word:
-            return word[1:-1]  # 去除引号
+            return word
 
 
 sniffer = PacketSniffer(interface, display_filter)
